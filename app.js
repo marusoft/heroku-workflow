02c3284c2aae2630e294c6298d141b4c71e59da0
marusoft <alimi.kmaruf@gmail.com>
2020-04-22 22:46:44 +0100

    modify the port number

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
